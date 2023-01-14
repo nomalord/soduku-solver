d07d3b3a884fb5576a9250b5a8b2f1c0589b929b
noam koushnir <nkoushnir@gmail.com>
2023-01-14 13:13:01 +0200

    barebones prototype of a sudoku solver, works for boards of all sizes prints to console not very quick

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Omega_Sudoku;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SudokuSolver sudokuSolver = new SudokuSolver(625);
+        sudokuSolver.Test();
+    }
+}
